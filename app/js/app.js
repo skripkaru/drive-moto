674a4fb1b3ba6f5265ac9a7177bf6de9cb027701
Evgeniy Skripkaru <skripkaru.evgeniy@gmail.com>
2021-01-20 20:32:31 +0300

    init

---
+++ app/js/app.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+	const mySwiper = new Swiper('.swiper-container', {
+		loop: true,
+		pagination: {
+			el: '.swiper-pagination',
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	})
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+const {src, dest, parallel, series, watch} = require('gulp');
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleancss = require('gulp-clean-css');
+const del = require('del');
+const svgSprite = require('gulp-svg-sprite');
+
+function browsersync() {
+   browserSync.init({
+      server: {baseDir: 'app/'},
+      notify: false,
+      online: true
+   });
+}
+
+function scripts() {
+   return src([
+      'node_modules/swiper/swiper-bundle.min.js',
+      'app/js/app.js',
+   ])
+      .pipe(concat('app.min.js'))
+      .pipe(uglify())
+      .pipe(dest('app/js/'))
+      .pipe(browserSync.stream());
+}
+
+function styles() {
+   return src([
+      'node_modules/swiper/swiper-bundle.min.css',
+      'node_modules/normalize.css/normalize.css',
+      'app/scss/app.scss',
+   ])
+      .pipe(sass())
+      .pipe(concat('app.min.css'))
+      .pipe(autoprefixer({overrideBrowserslist: ['last 10 versions'], grid: true}))
+      .pipe(cleancss(({level: {1: {specialComments: 0}}/* , format: 'beautify' */})))
+      .pipe(dest('app/css/'))
+      .pipe(browserSync.stream());
+}
+
+function svgSprites() {
+   return src('app/svg/**/*.svg') // svg files for sprite
+      .pipe(svgSprite({
+         mode: {
+            stack: {
+               sprite: "../sprite.svg"  //sprite file name
+            }
+         },
+      }
+      ))
+      .pipe(dest('app/svg/sprite/'));
+}
+
+function cleandist() {
+   return del('dist/**/*', {force: true});
+}
+
+function buildcopy() {
+   return src([
+      'app/**/*.html',
+      'app/css/**/*.min.css',
+      'app/js/**/*.min.js',
+      'app/fonts/**/*.woff2',
+      'app/svg/**/*.svg',
+      'app/img/**/*',
+   ], {base: 'app'})
+      .pipe(dest('dist'));
+}
+
+function startwatch() {
+   watch(['app/**/**/*.scss'], styles);
+   watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
+   watch('app/**/*.html').on('change', browserSync.reload);
+}
+
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.styles = styles;
+exports.build = series(cleandist, styles, scripts, buildcopy);
+exports.default = parallel(styles, scripts, browsersync, startwatch);
